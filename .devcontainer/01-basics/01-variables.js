91d4ff7481b0d0db768b144c6e55d3b7ea1c4304
amnabibi869 <amnafayyaz808@gmail.com>
2025-09-21 11:28:24 +0000

    varaible doone

---
+++ .devcontainer/01-basics/01-variables.js
@@ -0,0 +1,16 @@
+const accountID = 144553
+let accountEmail = "amna@google.com"
+var accountPassword = "12345"
+accountCity = "rwp"
+
+//accountId = 2 //not allowed
+/*
+prefer not to use var
+beacause of iisue(isue esy k agr acountid 10 place pr hmny likhi
+ to kisi ek accountid ko change krny say wo sari ko change kr deta tha ) 
+ in block scope means "{}" braces and functinal scope
+
+*/
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
