1f0f1c7c7ac9fa06a3cbdb364ad0f5f44dc8797f
amnabibi869 <amnafayyaz808@gmail.com>
2025-09-22 10:39:13 +0000

    datatype

---
+++ .devcontainer/01-basics/02-datatype.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as nwer version
+// alert(3+3) // we are using node.js not browser
+let name ="amna" // string 
+let age = 13 // number (2 to  powr 53)
+let isLoggedIn = false 
+let state; // undefined hy ye
+//bigin
+//boolean => true/false
+//null => standalone value
+//symbol => unique
+
+   
+//object
+console.log(typeof age);// ye bta dy ga k ye konsa varaible type hy 
