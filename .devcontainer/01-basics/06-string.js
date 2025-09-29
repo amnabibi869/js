c9e572310325c06942a2ac26f02ee38a3126ff6a
amnabibi869 <amnafayyaz808@gmail.com>
2025-09-29 10:44:27 +0000

    string

--- .devcontainer/01-basics/06-string.js
+++ .devcontainer/01-basics/06-string.js
@@ -14,7 +14,7 @@ const gameName = new String('hitesh-hc-com')
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));//tell the positioniu
+console.log(gameName.indexOf('t'));//tell the position
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
@@ -24,12 +24,12 @@ console.log(anotherString);
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());//Spaces khtm krny k liye output may
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar'))//found kr k deta hy value
 
 console.log(gameName.split('-'));
